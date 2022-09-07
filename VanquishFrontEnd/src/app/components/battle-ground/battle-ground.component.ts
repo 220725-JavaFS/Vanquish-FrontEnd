28b5b033f367b7d5702ae0fc8f710f77bd3dd691
HunterKelly <Hunterkelly1234@gmail.comgit pullgit config --global user.email Hunterkelly1234@gmail.com>
2022-09-06 19:09:23 -0500

    added weather API

--- VanquishFrontEnd/src/app/components/battle-ground/battle-ground.component.ts
+++ VanquishFrontEnd/src/app/components/battle-ground/battle-ground.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Account } from '~/models/account';
 
 @Component({
   selector: 'app-battle-ground',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BattleGroundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {  }
 
   ngOnInit(): void {
+    let City: string;
+    let weather: string;
+    //Assign city from player profile city location
+    City = Account.city;
+    weather = this.getWeather(City);
+
+    
   }
 
+  getWeather(location: string) {
+    let JSONweather = this.httpClient.get('http://api.weatherstack.com/current?access_key=a070930780ba3b2ea3632e06e6507f4a&query=' + location);
+    let JSONObject = JSON.stringify(JSONweather);
+    return JSONObject;
+    
+
+  }
 }
--- VanquishFrontEnd/src/app/models/account.ts
+++ VanquishFrontEnd/src/app/models/account.ts
@@ -2,5 +2,5 @@ export class Account {
 
     constructor(public id:number, public firstName:string, public lastName:string,
         public username:string, public userPwd:string,
-        public email:string, public zipcode:number){}
+        public email:string, public city:string){}
 }
