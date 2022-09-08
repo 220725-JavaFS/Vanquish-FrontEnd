8e1a91b6805324e3347779d38ce5239b18e50274
BlizWoRo <saumik148@revature.net>
2022-09-08 16:48:18 -0500

    added default image

--- VanquishFrontEnd/src/app/components/player-profile/player-profile.component.ts
+++ VanquishFrontEnd/src/app/components/player-profile/player-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-player-profile',
@@ -6,15 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-profile.component.css']
 })
 export class PlayerProfileComponent implements OnInit {
-  ImagePath: string;
+  ImagePath: string ='/assets/images/Default.png';
 
-  constructor() { 
+  constructor(private accountService:AccountService) { 
     //switch statement or if else statement depending on credentials and database
     this.ImagePath = '/assets/images/Warrior.png'
+    //this.ImagePath = '/assets/images/Default.png'
     
   }
 
   ngOnInit(): void {
+  }
+
+  private getCharacter(character:string){
+    //this.accountService.getAccountByID().getCharacter()
   }
 
 }
